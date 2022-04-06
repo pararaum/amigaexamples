a9d16fe242e1779bb4989fdf6105eef3b249d3e5
Pararaum <pararaum@gmail.com>
2022-04-06 21:14:04 +0200

    add dual playfield

---
+++ mixer/config.mk
@@ -0,0 +1,8 @@
+AFLAGS=-m68000 -opt-speed
+AS=vasmm68k_mot -Fhunk -I$$NDK39_ASMINC -I$$T7D/asminc
+CC=vc +kick13 -I$$NDK39_INCLUDE -I$$T7D/include
+CFLAGS=-c99 -speed -O3 -DNDEBUG
+LD=vc +kick13
+LDFLAGS=$(LIBS)
+LIBS=-L$$T7D -lamiga -lt7d
+VLINKFLAGS=-s -L$$NDK39/Include/linker_libs
