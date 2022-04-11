1266dc3b80fa3a7c409bc8ca35accf5e187c5a6a
Pararaum <pararaum@gmail.com>
2022-04-11 17:25:50 +0200

    first version of track load

--- examples/simple_bootblock/boot_and_load_track.s
+++ examples/simple_bootblock/boot_and_load_track.s
@@ -1,8 +1,11 @@
 
 	include	hardware/custom.i
 	include	hardware/dmabits.i
+	include	hardware/intbits.i
+	include	hardware/cia.i
 
 BOOTCODEADDRESS = $100
+CIAF_DSKSELx	EQU	(CIAF_DSKSEL3|CIAF_DSKSEL2|CIAF_DSKSEL1|CIAF_DSKSEL0)
 
 	;; The following are added by  xdftool.
 	;dc.b	"DOS",0		; Header of a bootable disk.
@@ -11,10 +14,14 @@ BOOTCODEADDRESS = $100
 
 ;;; Registers:
 	;; A5 = $DFF000
+	;; A4 = CIA-B $BFD100
+	;; A3 = CIA-A $BFE001
 
 	;; In A6 we have the Exec base.
 	jsr	_LVOForbid(a6)	  ; No more task switching.
-	lea	$DFF000,a5	; Custom base in A5.
+	lea.l	$DFF000,a5	; Custom base in A5.
+	lea.l	$BFD100,a4
+	lea.l	$BFE001,a3
 	move.w	#$7fff,intena(a5) ; Disable interrupts.
 	move.w	#$7fff,intreq(a5) ; Disable interrupt requests.
 	move.w	#$7fff,dmacon(a5) ; Disable DMA.
@@ -27,11 +34,51 @@ BOOTCODEADDRESS = $100
 	jmp	BOOTCODEADDRESS.w ; Now continue below:
 
 bootcode:
+	;; Turn display on.
 	lea.l	coplist(pc),a0
 	move.l	a0,cop1lc(a5)	; Point copper to the Copperlist.
 	;; Enable display and copper.
 	move.w	#DMAF_SETCLR|DMAF_COPPER|DMAF_RASTER|DMAF_MASTER,dmacon(a5)
+	;; Now activate vblank interrupt.
+	lea.l	vblank_handler(pc),a0
+	move.l	a0,$6c.w	; Level 3 interrupt.
+	; Enable vblank interrupt.
+	move.w	#INTF_SETCLR|INTF_INTEN|INTF_VERTB,intena(a5)
+	moveq	#120,d0		; Wait
+	bsr	waitframes
+	;; Check if on track zero otherwise: kaputt!
+	;; http://cyberpingui.free.fr/tuto_trackloader.htm
+	;; Bit 4 = TRACK0
+	btst	#4,(a3)
+	bne.s	.OK
+	illegal
+.OK:	
+	;; Turn Motor on.
+	or.b	#CIAF_DSKSELx,(a4)	; deselect all drives
+	bclr.b	#CIAB_DSKMOTOR,(a4)
+	bclr.b	#CIAB_DSKSEL0,(a4)	; select drive 0
+	move	#500/20,d0		; Wait 500 ms.
+	bsr	waitframes
 	bra	*		; Stay a while! Stay forever!
+
+;;; Input: d0=frames to wait.
+waitframes:
+	lea.l	framecounter(pc),a0
+	move.w	d0,(a0)
+.l1:	tst.w	(a0)
+	bpl	.l1
+	rts
+
+vblank_handler:
+	movem.l	d0-a6,-(sp)
+	lea	$Dff000,a5
+	lea.l	framecounter(pc),a0
+	subq.w	#1,(a0)
+	move	framecounter(pc),color(a5)
+	move.w	#INTF_VERTB,intreq(a5) ; Acknowledge interrupt.
+	movem.l	(sp)+,d0-a6
+	rte
+
 
 	dc.l	"####"
 coplist:
@@ -50,3 +97,4 @@ coplist:
 	dc.l	-2		; Wait forever: $FFFF,$FFFE
 	dc.b	"Pararaum/T7D",0
 	even
+framecounter:	dc.w	0
