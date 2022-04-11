4d17c5f3b40178f6ecd397e16221f69cf786dabb
Pararaum <pararaum@gmail.com>
2022-04-11 16:16:03 +0200

    set colour in copperlist

--- examples/simple_bootblock/boot_and_do_nothing.s
+++ examples/simple_bootblock/boot_and_do_nothing.s
@@ -12,12 +12,9 @@
 	;; In A6 we have the Exec base.
 	jsr	_LVOForbid(a6)	  ; No more task switching.
 	lea	$DFF000,a5	; Custom base in A5.
-	move.w	#$7fff,intena(a5) ; Disable interrupts
-	move.w	#$7fff,intreq(a5) ; Disable interrupt requests
+	move.w	#$7fff,intena(a5) ; Disable interrupts.
+	move.w	#$7fff,intreq(a5) ; Disable interrupt requests.
 	move.w	#$7fff,dmacon(a5) ; Disable DMA.
-
-	move.w	#$012e,color(a5) ; Light blue background.
-	move.w	#$0efe,color+2(a5) ; No quite white foreground.
 
 	lea.l	coplist(pc),a0
 	lea.l	$100.w,a1
@@ -32,16 +29,17 @@
 
 	dc.l	"END"
 coplist:
-        dc.w    $0106,$0000,$01fc,$0000         ; AGA compatible
-        ;; Setting up display.
-        ;; see also: http://cyberpingui.free.fr/tuto_graphics.htm
-        dc.w	diwstrt,$2c81,diwstop,$2cc1         ; DIWSTRT/DIWSTOP
-        dc.w	ddfstrt, $0038,ddfstop,$00d0         ; DDFSTRT/DDFSTOP
-	dc.w	bplcon0,$8000|(1<<12)|$200	     ; HiRes and one bitplane.
-        dc.w    $0102,$0000,$0104,$0000
-        dc.w    $0106,$0000
-	dc.w	bpl1mod,$0000
-        dc.w    bpl2mod,$0000
+	dc.w	$0106,$0000,$01fc,$0000		; AGA compatible
+	;; Setting up display.
+	;; see also: http://cyberpingui.free.fr/tuto_graphics.htm
+	dc.w	diwstrt,$2c81,diwstop,$2cc1	; DIWSTRT/DIWSTOP
+	dc.w	ddfstrt, $0038,ddfstop,$00d0	; DDFSTRT/DDFSTOP
+	dc.w	bplcon0,$8000|(1<<12)|$200	; HiRes and one bitplane.
+	dc.w	bplcon1,$0000,bplcon2,$0000
+	dc.w	bplcon3,$0000
+	dc.w	bpl1mod,$0000,bpl2mod,$0000
+	dc.w	color+0,$012e		  ; Light blue background.
+	dc.w	color+2,$0efe		  ; Not quite white foreground.
 	dc.w	bplpt,$0000,bplpt+2,$0000 ; Bitplane pointer points to $0.
 	dc.l	-2		; Wait forever: $FFFF,$FFFE
 	dc.b	"Pararaum/T7D",0
