7fb224b5446f705fb38299b9fcba9f1594d2c290
Pararaum <pararaum@gmail.com>
2022-08-30 22:12:15 +0200

    add function prototype

--- framework/include/t7d/default_irq_routine.h
+++ framework/include/t7d/default_irq_routine.h
@@ -1,4 +1,23 @@
 
 extern volatile unsigned long framecounter;
-extern void(*irq_routine_funpointer)(void);
+
+extern volatile void(*irq_routine_funpointer)(void);
+
+/*! \brief Setup the default interrupt routine
+ *
+ * The default interrupt routine checks for the mouse button and irqfun once per frame
+ *
+ * \param initfun called once before anything else
+ * \param entry called to start the demo (with enabled interrupts)
+ * \param irqfun called once per frame
+ */
 void set_irq_routine(void(*initfun)(void), void (*entryfun)(void), void (*irqfun)(void));
+
+/*! \brief set the IRQ routine function pointer to be called
+ *
+ * At each frame this function is called. This replaces the old stored function pointer as optimisation tends to ignore the volatile sometimes. So we burn 32 cycles.
+ *
+ * \param irqfun function to be called from irq every frame
+ * \returns old function pointer
+ */
+void *set_irq_routine_funpointer(__reg("a0") void(*irqfun)(void));
