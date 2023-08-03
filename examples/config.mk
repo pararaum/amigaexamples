9de72df4ecd3363dea0207a84a9380992fd7dc1d
Pararaum <pararaum@gmail.com>
2023-08-03 21:41:51 +0200

    run in priviledged mode

--- examples/config.mk
+++ examples/config.mk
@@ -1,5 +1,5 @@
 AFLAGS=-m68000 -opt-speed
-AS=vasmm68k_mot -Fhunk -I$$NDK39_ASMINC -I$$T7D/asminc
+AS=vasmm68k_mot -Fhunk -I$$NDK39_ASMINC -I$$T7D/asminc -L $@.lst
 CC=vc +kick13 -I$$NDK39_INCLUDE -I$$T7D/include
 CFLAGS=-c99 -speed -O3 -DNDEBUG
 LD=vc +kick13
