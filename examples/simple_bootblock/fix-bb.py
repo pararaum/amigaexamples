8c79341823fc3f118f2f6bc646d90334f024d71e
Pararaum <pararaum@gmail.com>
2026-07-07 22:14:11 +0200

    add full track 0 control

--- examples/simple_bootblock/fix-bb.py
+++ examples/simple_bootblock/fix-bb.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import sys
 import struct
@@ -7,18 +7,18 @@ def main(fname):
     with open(fname, 'rb+') as fin:
         data = fin.read()
         assert len(data) == 880 * 512 * 2
-	chksum = 0x444f5300
-	for w in range(8, 1024, 4):
-	    chksum += struct.unpack('>I', data[w:w + 4])[0]
-	    if chksum > 0xffffffff:
-		chksum = (chksum + 1) & 0xffffffff
-	chksum = (~chksum) & 0xffffffff
+        chksum = 0x444f5300
+        for w in range(8, 1024, 4):
+            chksum += struct.unpack('>I', data[w:w + 4])[0]
+            if chksum > 0xffffffff:
+                chksum = (chksum + 1) & 0xffffffff
+        chksum = (~chksum) & 0xffffffff
         fin.seek(4)
         fin.write(struct.pack('>I', chksum))
-    print("Checksum = $%08X" % chksum)
+        print("Checksum = $%08X" % chksum)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-	main(sys.argv[1])
+        main(sys.argv[1])
     else:
-	sys.stderr.write('fix-bb <filename>\n')
+        sys.stderr.write('fix-bb <filename>\n')
