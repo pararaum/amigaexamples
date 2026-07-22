9e6ad25345ab63999c05d43c474b6f7b4f357a55
Pararaum <pararaum@gmail.com>
2026-07-22 22:14:06 +0200

    remove obsolete code from part0

--- examples/simple_bootblock/resident.c
+++ examples/simple_bootblock/resident.c
@@ -4,13 +4,24 @@
 #include <t7d/customchips.h>
 #include "boss.h"
 
+#define NPARTS 1
+
 extern volatile struct Custom custom;
 extern volatile uint16_t framecounter;
 
 int main() {
   void *nextpart;
+  int partcounter;
   
   BossWriteln("Loading parts.");
-  nextpart = BossManifestLoad(0x47465832);
+  for(partcounter = 0; partcounter < NPARTS; ++partcounter) {
+    if((partcounter & 1) == 0) {
+      BossWriteln("Loading even part at $C20000.");
+    } else {
+      BossWriteln("Loading odd part at $c50000.");
+    }
+    nextpart = BossManifestLoad(0x50543030);
+    BossPrintHex((uint32_t)nextpart);
+  }
   return 0;
 }
