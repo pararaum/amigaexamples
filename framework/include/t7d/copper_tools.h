5afeb7aea54a5a9ca4f6f64f04d18d2208317528
Pararaum <pararaum@gmail.com>
2026-08-21 11:43:53 +0200

    add another copper macro

--- framework/include/t7d/copper_tools.h
+++ framework/include/t7d/copper_tools.h
@@ -9,6 +9,6 @@
 #define COPPERPTRMOVE32(ptr, reg, val) *ptr++ = reg; *ptr++ = (val)>>16; *ptr++ = (reg)+2; *ptr++ = (val)&0xFFFF;
 #define COPPERPTRWAIT(ptr, vpos, hpos) *ptr++ = ((vpos)<<8)|(hpos)|1; *ptr++ = 0xfffe;
 #define COPPERPTRBLITFIN(ptr) *ptr++ = 0x101; *ptr++ = 0x7ffe;
-
+#define COPPERPTREND(ptr) *ptr++ = 0xFFFFu; *ptr++ = 0xFFFEu;
 
 #endif
